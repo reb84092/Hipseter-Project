f1964d54d8f4ed8a7fbb64e9cb565bdf56f135ac
reb84092 <rbar3155@740s-pl-102.local>
2015-11-19 09:20:54 -0700

    Fixed the runner

--- src/hipster/controller/HipsterRunner.java
+++ src/hipster/controller/HipsterRunner.java
@@ -2,5 +2,9 @@ package hipster.controller;
 
 public class HipsterRunner
 {
-
+	public static void main (String [] args)
+	{
+		HipsterController myController = new HipsterController();
+		myController.start();
+	}
 }
